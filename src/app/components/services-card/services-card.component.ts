16414a7a77699f2d2a7c8594753585abdb3c10bd
Silas Coley <silascoley3@gmail.com>
2023-04-30 22:24:51 -0500

    bundles-and-combos-update

--- src/app/components/services-card/services-card.component.ts
+++ src/app/components/services-card/services-card.component.ts
@@ -6,17 +6,17 @@ import { PocketbaseService } from 'src/app/pocketbase.service';
   templateUrl: './services-card.component.html',
   styleUrls: ['./services-card.component.css']
 })
-export class ServicesCardComponent implements OnInit {
+export class ServicesCardComponent {
 
   bundles: any;
 
   constructor(private database: PocketbaseService){}
 
-  ngOnInit(): void {
-    this.database.getPocketBaseData().then(data => {
-      this.bundles = data.items.filter((product: any) => product.type === 'Seasonings');
-    });
-  }
+  // ngOnInit(): void {
+  //   this.database.getPocketBaseData().then(data => {
+  //     this.bundles = data.items.filter((product: any) => product.type === 'Seasonings');
+  //   });
+  // }
 
   services = [ 
     {
