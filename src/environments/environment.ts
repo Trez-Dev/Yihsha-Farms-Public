815a411f3bfb49d5e852173004874a703895a723
Silas Coley <silascoley3@gmail.com>
2023-05-23 15:12:43 -0500

    environments

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    AVATAR_API_KEY: 'uAGaLn8FwOfY4Z',
-    LECOY_ADMIN_ID: 'o070vnbkqt330nj',
-    SILAS_ADMIN_ID: 'ir9gon2r5kpvr50',
-    PAYPAL_CLIENT_ID: 'AU_gR6MotvbdS2NNxiJu7PlNu_5Rz2KIWV18upJWvxNzPEud-EQHFxoDxFgAy_SIK3vOZSVbS5vMpRBa',
-    REDIRECT_URL: 'http://localhost:4200/redirect',
-    POCKETBASE_REST_API: 'https://yihsha-farms.pockethost.io'
-};
